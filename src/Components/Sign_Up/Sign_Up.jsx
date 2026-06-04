1e05001c3a1f12c5c00548565889e790a0fc6c1c
webdevCrafts <tyemang@gmail.com>
2026-06-04 18:17:58 -0400

    still trying to add the login functionality

--- src/Components/Sign_Up/Sign_Up.jsx
+++ src/Components/Sign_Up/Sign_Up.jsx
@@ -62,19 +62,19 @@ const Sign_Up = () => {
                 <div className="signup-text"> 
                     <h1>Sign Up</h1>
                 </div>
-                <div className="signup-text1" style={{textAlign: 'left'}}> 
+                <div className="signup-text1"> 
                     Already a member? <span><Link to="/Login" style={{color: '#2190FF'}}> Login</Link></span>
                 </div>
                 <div className="signup-form">
                     <form method="POST" onSubmit={register}>
                         <div className="form-group">
+                            <label htmlFor="name">Full Name</label>
+                            <input value={name} type="text" onChange={(e) => setName(e.target.value)} name="name" id="name" className="form-control" pattern="[a-zA-Z ]{1,15}" title="must only be letters" placeholder="Enter your name" aria-describedby="helpId" />
+                        </div>
+                        <div className="form-group">
                             <label htmlFor="email">Email</label>
                             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" title="must be valid email format" placeholder="Enter your email" aria-describedby="helpId" />
                             {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Full Name</label>
-                            <input value={name} type="text" onChange={(e) => setName(e.target.value)} name="name" id="name" className="form-control" pattern="[A-Z]{1}[a-z]{1,}" title="must only be letters" placeholder="Enter your name" aria-describedby="helpId" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="phone">Phone</label>
@@ -82,12 +82,14 @@ const Sign_Up = () => {
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" className="form-control" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="must be at least 8 chars long and has numbers and letters" placeholder="Enter your password" aria-describedby="helpId" />
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" className="form-control" pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}" title="must be at least 8 chars long and has numbers and letters" placeholder="Enter your password" aria-describedby="helpId" />
                         </div>
 
                         <div className="btn-group"> 
-                            <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button> 
-                            <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button> 
+                            {/* <button className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button> 
+                            <button className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>  */}
+                            <input type="submit" id="submit" value="submit"/>
+                            <input type="reset" id="reset" value="reset" />
                         </div>
                     </form>
                 </div>
