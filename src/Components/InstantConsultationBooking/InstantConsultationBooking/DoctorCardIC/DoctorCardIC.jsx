bf2f000d4e3dded46c52f299b5ef43369b79804c
webdevCrafts <tyemang@gmail.com>
2026-06-05 12:16:31 -0400

    made search functionality for instant consultation work

--- src/Components/InstantConsultationBooking/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.jsx
+++ src/Components/InstantConsultationBooking/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.jsx
@@ -33,7 +33,8 @@ const DoctorCardIC = ({ name, speciality, experience, ratings, profilePic }) => 
     <div className="doctor-card-container">
       <div className="doctor-card-details-container">
         <div className="doctor-card-profile-image-container">
-        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg>
+        {/* <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg> */}
+        <img src='https://cdn.pixabay.com/photo/2020/07/31/15/33/face-5453349_1280.png' alt="doc pic"/>
         </div>
         <div className="doctor-card-details">
           <div className="doctor-card-detail-name">{name}</div>
